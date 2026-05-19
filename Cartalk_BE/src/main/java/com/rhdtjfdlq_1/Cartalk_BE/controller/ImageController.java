b8a11ff5c4c223bc2fab539cb3132974561fe925
박강윤 <pky86443531@gmail.com>
2026-05-19 11:31:45 +0900

    이미지 전송 수정

---
+++ Cartalk_BE/src/main/java/com/rhdtjfdlq_1/Cartalk_BE/controller/ImageController.java
@@ -0,0 +1,63 @@
+package com.rhdtjfdlq_1.Cartalk_BE.controller;
+
+import com.rhdtjfdlq_1.Cartalk_BE.dto.ResponseImageUploadDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/images")
+public class ImageController {
+
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
+    @PostMapping(value = "/chat", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseImageUploadDto uploadChatImage(
+            @RequestPart("image") MultipartFile image
+    ) throws IOException {
+
+        if (image.isEmpty()) {
+            throw new IllegalArgumentException("EMPTY_FILE");
+        }
+
+        String contentType = image.getContentType();
+
+        if (contentType == null ||
+                (!contentType.equals("image/jpeg")
+                        && !contentType.equals("image/png")
+                        && !contentType.equals("image/jpg"))) {
+
+            throw new IllegalArgumentException("INVALID_FILE_TYPE");
+        }
+
+        File directory = new File(uploadDir);
+
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+
+        String extension =
+                StringUtils.getFilenameExtension(image.getOriginalFilename());
+
+        String fileName = UUID.randomUUID() + "." + extension;
+
+        // 여기 수정
+        File targetFile = new File(directory, fileName);
+
+        image.transferTo(targetFile);
+
+        String imageUrl =
+                "http://localhost:8080/uploads/" + fileName;
+
+        return new ResponseImageUploadDto(imageUrl);
+    }
+}
---
+++ Cartalk_BE/src/main/java/com/rhdtjfdlq_1/Cartalk_BE/dto/ResponseImageUploadDto.java
@@ -0,0 +1,11 @@
+package com.rhdtjfdlq_1.Cartalk_BE.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ResponseImageUploadDto {
+
+    private String imageUrl;
+}
