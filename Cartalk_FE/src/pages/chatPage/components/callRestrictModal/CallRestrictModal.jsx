d3c20c26616a41320340846488de2616b2a5a6b6
leeeeeseojin <kkyuuu00@gmail.com>
2026-05-23 05:57:05 +0900

    fix: close modal on outside click

--- Cartalk_FE/src/pages/chatPage/components/callRestrictModal/CallRestrictModal.jsx
+++ Cartalk_FE/src/pages/chatPage/components/callRestrictModal/CallRestrictModal.jsx
@@ -2,9 +2,9 @@ import './CallRestrictModal.css'
 
 import chatWarnIcon from '../../../../assets/chat/chat-warn.svg'
 
-export default function CallRestrictModal() {
+export default function CallRestrictModal({ onClose }) {
   return (
-    <div className='callrestrict-toast'>
+    <div className='callrestrict-toast' onClick={onClose}>
       <img className='callrestrict-toast__icon' src={chatWarnIcon} alt='경고' />
       <p className='callrestrict-toast__text'>
         하루 통화 가능 횟수를 초과하여 더 이상 통화가 불가능합니다
